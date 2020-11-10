7830bc36410f724fa5f06021904cd5476bdcc4ad
Ashish Kumar <akmashish15@gmail.com>
2020-11-10 11:42:57 +0530

    adding starting project

---
+++ script.js
@@ -0,0 +1,42 @@
+(4 + 3;)
+if(4 + 3 === 7) {
+	alert("you are smart")
+ }
+function sayhello () {
+	console.log ("hello");
+}
+sayhello();
+
+var saybye = function() {
+	console.log("bye");
+}
+saybye();
+
+
+
+
+function multiply (a, b) {
+	return a * b
+}
+
+multiply (5, 10);
+
+
+
+
+
+function multiply (a, b) {
+if (a > 10 || b > 10) {
+	return "that's too hard";
+} else {
+	return a*b;
+	}
+}
+
+multiply (5, 10);
+
+
+
+
+
+
